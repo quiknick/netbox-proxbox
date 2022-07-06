17e4c693a1570111608dfdbcc3c8e5d04a840414
Javier Alejandro Ruiz <javier.ruiz@edgeuno.com>
2022-07-06 09:34:10 -0500

    feature(Queue): updated the rq sh file

--- rq.sh
+++ rq.sh
@@ -5,5 +5,5 @@
 /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py rqworker high default low netbox_proxbox.netbox_proxbox &
 /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py rqworker high default low netbox_proxbox.netbox_proxbox &
 
-/opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py rqscheduler high default low netbox_proxbox.netbox_proxbox &
+/opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py rqscheduler high default low netbox_proxbox.netbox_proxbox
 exec "$@"
